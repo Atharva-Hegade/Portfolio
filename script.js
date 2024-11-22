88dd8949c6abdd7f48c61ee05b7188a94f802f21
Atharva-Hegade <atharvajhegade@gmail.com>
2024-11-22 19:58:44 +0530

    contact-form

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
+function sendMail() {
+
+    let parms = {
+
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+
+    }
+
+    emailjs.send("service_1qgnipa" , "template_qxj69jk" , parms).then(alert("Email Sent!"))
 }
+
